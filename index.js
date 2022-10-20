b45e4b005f7a612e3d0934ce4cd48fc533994fc9
Nickolas Taylor <111876215+marginnickolas@users.noreply.github.com>
2022-10-20 12:17:53 -0500

    Finished html and javascript. Counter works. Css is next to finish

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+let homeScore = 0
+let guestScore = 0
+let guest = document.getElementById("guest-score")
+let home = document.getElementById("home-score")
 
+function add1Home() {
+    homeScore += 1
+    score()
+}
+function add2Home() {
+    homeScore += 2
+    score()
+}
+function add3Home()  {
+    homeScore += 3
+    score()
+}
+function add1Guest() {
+    guestScore += 1
+    score()
+}
+function add2Guest() {
+    guestScore += 2
+    score()
+}
+function add3Guest() {
+    guestScore += 3
+    score()
+}
+
+function score() {
+    guest.textContent = guestScore
+    home.textContent = homeScore
+}
